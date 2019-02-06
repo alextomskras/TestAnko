46218aa128e3ad7cf0382bc8b48a22f567fff00d
FESS <dreamer.alex.toms@gmail.com>
2019-02-06 22:19:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testanko/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.testanko
+
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.setContentView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MainActivityUI().setContentView(this)
+    }
+}
